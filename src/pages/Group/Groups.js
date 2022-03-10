47ad0d1bfa417c3c2ba163adc5fb030c8ec726ae
Benrobo <alumonabenaiah71@gmail.com>
2022-03-09 19:30:41 -0800

    added group component

--- src/pages/Group/Groups.js
+++ src/pages/Group/Groups.js
@@ -6,6 +6,8 @@ import "./style.css"
 import Layout from '../../components/Layout/Layout'
 import TopNavbar from '../../components/TopNavbar/Top'
 
+// import { } from "react-icons/fa"
+import { BiPlus } from "react-icons/bi"
 
 function Groups() {
 
@@ -15,12 +17,40 @@ function Groups() {
             <MainCont>
                 <TopNavbar activeBar="Groups" />
 
-                <h4>Groups</h4>
-                <hr />
-                <br />
+                <div className="group-list">
+                    <div className="left">
+                        <button className="btn create">
+                            <BiPlus className="icon" />
+                            Create Group
+                        </button>
+                    </div>
+                    <div className="right">
+                        {Array.from("sdcdsc").map((list, i) => {
+                            return (
+                                <div className="group-card" key={i}>
+                                    <img src={`https://avatars.dicebear.com/api/initials/group-${i}.svg`} alt="" />
+                                    <div className="more">
+                                        <p>Group Falcon</p>
+                                        <span className="mem">3</span>
+                                        <br />
+                                        <button className="btn view-member">view members</button>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
             </MainCont>
         </Layout>
     )
 }
 
 export default Groups
+
+
+
+
+function CreateGroup() {
+
+}
+
